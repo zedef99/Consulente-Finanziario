6e365fd3147e1d9506d2db256533b2f56000910f
Federico Stragliotto <f.stragliotto@adacta.it>
2024-09-13 12:56:14 +0200

    cambio struttura repository

---
+++ Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="👋",
+    layout="wide"
+)
+
+st.write("# Consulente Finanziario👋")
+
+st.markdown(
+    """
+    Questo consulente finanziario offre l'opportunità di interrograre un file condiviso e di reperire i prezzi di chiusura delle azioni.
+"""
+)
